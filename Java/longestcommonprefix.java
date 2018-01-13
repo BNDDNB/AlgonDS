9fd9d81e2c6882af42ce44852a7126888ae39ae9
bnddnb <11622931+BNDDNB@users.noreply.github.com>
2018-01-12 23:52:28 -0500

    longestcommonprefix

---
+++ Java/longestcommonprefix.java
@@ -0,0 +1,10 @@
+//longest common prefix
+//can be scanned horizontally or vertically depending on 
+//this implements the vertical scanning
+
+public static String solver(String [] ss){
+	if (ss.length == 0){return "";}
+	for (int j = 0; j < ss[0].length(); j++){
+		
+	}
+}
