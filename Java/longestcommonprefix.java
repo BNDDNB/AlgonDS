fe87bb36bfcc0febcb78af0f96d27225793d87f4
bnddnb <11622931+BNDDNB@users.noreply.github.com>
2018-01-12 23:59:53 -0500

    update

--- Java/longestcommonprefix.java
+++ Java/longestcommonprefix.java
@@ -5,6 +5,11 @@
 public static String solver(String [] ss){
 	if (ss.length == 0){return "";}
 	for (int j = 0; j < ss[0].length(); j++){
-		
+		String tmp = ss[0].charAt(j);
+		for(int i = 0; i < ss.length; i++){
+			if(ss[i].length() == j || ss[i].charAt(j)!=tmp){return ss[i].substring(0,j);}
+		}
 	}
+	//this is equivalent of the 0th string being the shortest
+	return str[0];
 }
